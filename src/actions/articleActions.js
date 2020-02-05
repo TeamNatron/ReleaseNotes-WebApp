7722ae5102955389fea52402c2ee5df2769f27c6
Lars <larsoyvind28@gmail.com>
2020-02-05 14:09:37 +0100

    add example thunk/axios action

--- src/actions/articleActions.js
+++ src/actions/articleActions.js
@@ -1,17 +1,34 @@
+import Axios from "axios";
+
 export const CREATE = "CREATE";
 export const UPDATE = "UPDATE";
 export const DELETE = "CREATE";
 
 export function updateArticle(payload) {
-    return {
-        type: UPDATE,
-        payload: payload
-    }
+  return {
+    type: UPDATE,
+    payload: payload
+  };
+}
+
+// expmple thunk/axios action
+// https://github.com/axios/axios
+// https://github.com/reduxjs/redux-thunk
+export function createArticleAsync(article) {
+  return dispatch => {
+    return Axios.create("/article", article)
+      .then(response => {
+        dispatch(createArticle(response));
+      })
+      .catch(error => {
+        throw error;
+      });
+  };
 }
 
 export function createArticle(article) {
-    return  {
-        type: CREATE,
-        payload: article
-    }
+  return {
+    type: CREATE,
+    payload: article
+  };
 }
