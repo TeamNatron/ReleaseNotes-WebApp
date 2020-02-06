a8b7461794c8de367f756a13da27bbacf8791567
Mordi490 <jananton007@hotmail.com>
2020-02-06 09:12:50 +0100

    footer added

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
-import React from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import "./App.css";
-import LandingScreen from "./components/screen/LandingScreen";
-import ReleaseNotesScreen from "./components/screen/ReleaseNotesScreen";
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import './App.css';
+import LandingScreen from './components/screen/LandingScreen';
+import ReleaseNotesScreen from './components/screen/ReleaseNotesScreen';
 
-import Navbar from "./components/Navbar";
-import { ThemeProvider } from "styled-components";
-import { GlobalStyle, theme } from "./styles/index";
-import { CssBaseline } from "@material-ui/core";
-import { Provider, useDispatch } from "react-redux";
-import { store } from "./setupStore";
-import ArticleScreen from "./components/screen/ArticleScreen";
+import Navbar from './components/Navbar';
+import { ThemeProvider } from 'styled-components';
+import { GlobalStyle, theme } from './styles/index';
+import { CssBaseline } from '@material-ui/core';
+import { Provider, useDispatch } from 'react-redux';
+import { store } from './setupStore';
+import ArticleScreen from './components/screen/ArticleScreen';
+import Footer from './components/Footer';
 
 function App() {
   return (
@@ -23,6 +24,7 @@ function App() {
           <Route path="/" exact component={LandingScreen} />
           <Route path="/releases" exact component={ReleaseNotesScreen} />
           <Route path="/articles/article01" exact component={ArticleScreen} />
+          <Footer />
         </ThemeProvider>
       </Router>
     </Provider>
