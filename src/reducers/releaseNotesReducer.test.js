a97fca2b108cb5a1a2edd07f34e12a20b2f61ae1
Mordi490 <jananton007@hotmail.com>
2020-03-02 13:37:19 +0100

    tests for release notes

--- src/reducers/releaseNotesReducer.test.js
+++ src/reducers/releaseNotesReducer.test.js
@@ -2,16 +2,48 @@ import { releaseNotes, releaseNote } from "./initialStates";
 import { releaseNotesReducer } from "./releaseNotesReducer";
 import {
   actions,
-  FETCH_RELEASENOTE_SUCCESS
+  FETCH_ALL_RELEASENOTES_SUCCESS
 } from "../actions/releaseNoteActions";
 
 describe("releaseNotes reducer", () => {
   it("should return the initial state", () => {
     expect(releaseNotesReducer(undefined, {})).toEqual(releaseNotes);
   });
+  it("should handle fetching all release notes", () => {
+    expect(
+      releaseNotesReducer(undefined, actions.fetchAllReleaseNotesPending())
+    ).toHaveProperty("pending", true);
+
+    expect(
+      releaseNotesReducer(
+        undefined,
+        actions.fetchAllReleaseNotesError("some error")
+      ).console.error
+    ).toEqual("some error");
+
+    expect(
+      releaseNotesReducer(
+        {
+          pending: false,
+          items: [],
+          error: null
+        },
+        {
+          type: FETCH_ALL_RELEASENOTES_SUCCESS,
+          payload: [{ name: "test-Version" }]
+        }
+      )
+    ).toEqual({
+      pending: false,
+      items: [{ name: "test-Version" }],
+      error: null
+    });
+  });
+  /*
   it("should ", () => {
     expect(
       releaseNotesReducer(undefined, actions.fetchReleaseNotePending())
     ).toHaveProperty("items");
   });
+  */
 });
