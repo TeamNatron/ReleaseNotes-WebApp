15915ca5132452e4bbf54bf88bf4f7aa9c8534d3
markusranda <randamarkus.95@gmail.com>
2020-03-03 15:13:50 +0100

    Added fetching for releases, and redux toolkit

--- src/reducers/combinedReducers.js
+++ src/reducers/combinedReducers.js
@@ -2,13 +2,13 @@ import { combineReducers } from "redux";
 import { articlesReducer } from "./articlesReducer";
 import { productsReducer } from "./productsReducer";
 import { productVersionsReducer } from "./productVersionsReducer";
-import { releaseNotesReducer } from './releaseNotesReducer';
-
+import { releaseNotesReducer } from "./releaseNotesReducer";
+import { releaseSlice } from "../slices/releaseSlice";
 
 export default combineReducers({
   articles: articlesReducer,
   products: productsReducer,
   productVersions: productVersionsReducer,
   releaseNotes: releaseNotesReducer,
-
+  releases: releaseSlice.reducer
 });
