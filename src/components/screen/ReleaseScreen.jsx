3f0259d563ece2be251831fc2003f5f0adfd9336
markusranda <randamarkus.95@gmail.com>
2020-03-09 12:23:01 +0100

    When routing to specific release, the  correct release gets rendered.

--- src/components/screen/ReleaseScreen.jsx
+++ src/components/screen/ReleaseScreen.jsx
@@ -1,11 +1,23 @@
-import React from "react";
+import React, { useEffect } from "react";
 import ReleaseView from "../releaseView/ReleaseView";
 import PageContainer from "../shared/PageContainer";
+import { useDispatch, useSelector } from "react-redux";
+import { findReleaseById } from "../../selectors/releaseEditorSelector";
+import { fetchReleaseById } from "../../slices/releaseSlice";
 
-const ReleaseScreen = () => {
+const ReleaseScreen = props => {
+  const dispatch = useDispatch();
+  const id = props.match.params.id;
+
+  useEffect(() => {
+    if (id) dispatch(fetchReleaseById(id));
+  }, [id]);
+
+  const release = useSelector(findReleaseById(id));
+
   return (
     <PageContainer>
-      <ReleaseView />
+      <ReleaseView release={release} />
     </PageContainer>
   );
 };
