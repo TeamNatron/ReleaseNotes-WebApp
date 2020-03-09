115e5bcd5c08f7b18ed2777b400828b2064bdb3b
Lars <larsoyvind28@gmail.com>
2020-03-09 12:30:38 +0100

    Improve release viewer styling

--- src/components/releaseView/FullReleaseNote.jsx
+++ src/components/releaseView/FullReleaseNote.jsx
@@ -1,17 +1,18 @@
 import React from "react";
 import HtmlWrapper from "./HtmlWrapper";
-import { Typography, Box } from "@material-ui/core";
+import { Box } from "@material-ui/core";
+import ReleaseH2 from "../shared/ReleaseH2";
 
 const FullReleaseNote = props => {
   return (
     <Box mb={1}>
       <HtmlWrapper
         gutterBottom={true}
-        variant="h4"
+        component={ReleaseH2}
         strip
         html={props.note.title}
       />
-      <HtmlWrapper variant="subtitle2" strip html={props.note.ingress} />
+      <HtmlWrapper variant="subtitle1" strip html={props.note.ingress} />
       <HtmlWrapper variant="body1" html={props.note.description} />
     </Box>
   );
