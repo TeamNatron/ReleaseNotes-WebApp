f3456de69aef90c779c4497158c5955ff02824ec
Lars <larsoyvind28@gmail.com>
2020-03-10 16:42:10 +0100

    create controller date picker component

---
+++ src/components/shared/ControlledDatePicker.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import PropTypes from "prop-types";
+import { DatePicker } from "@material-ui/pickers";
+/**
+ *
+ */
+const ControlledDatePicker = props => {
+  return (
+    <DatePicker
+      disableToolbar
+      variant="inline"
+      format="LL"
+      margin="normal"
+      id="date-picker-inline"
+      label={props.label}
+      value={props.value}
+      onChange={props.setValue}
+    />
+  );
+};
+
+export default ControlledDatePicker;
+
+ControlledDatePicker.propTypes = {
+  value: PropTypes.object,
+  setValue: PropTypes.func,
+  label: PropTypes.string
+};
