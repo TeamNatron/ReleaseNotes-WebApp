5e1ec2a3d36661e729ea8f4a5852fc0b854a9ee5
Lars <larsoyvind28@gmail.com>
2020-03-11 11:44:14 +0100

    Update ReleaseViewScreen looks

--- src/components/screen/ReleaseScreen.jsx
+++ src/components/screen/ReleaseScreen.jsx
@@ -4,6 +4,9 @@ import PageContainer from "../shared/PageContainer";
 import { useDispatch, useSelector } from "react-redux";
 import { findReleaseById } from "../../selectors/releaseEditorSelector";
 import { fetchReleaseById } from "../../slices/releaseSlice";
+import ScreenTitle from "../shared/ScreenTitle";
+import Ingress from "../shared/Ingress";
+import SpacedDivider from "../shared/SpacedDivider";
 
 const ReleaseScreen = props => {
   const dispatch = useDispatch();
@@ -11,12 +14,24 @@ const ReleaseScreen = props => {
 
   useEffect(() => {
     if (id) dispatch(fetchReleaseById(id));
-  }, [id]);
+  }, [id, dispatch]);
 
   const release = useSelector(findReleaseById(id));
+  const productVersion =
+    release?.productVersion.product.name +
+    " " +
+    release?.productVersion.version;
 
+  const title = "Revisjonsnytt " + productVersion;
+  const ingress =
+    "Her finner du informasjon om de vikstigste endringene/korreksjonene i " +
+    productVersion +
+    ".";
   return (
     <PageContainer>
+      <ScreenTitle>{release ? title : ""}</ScreenTitle>
+      <Ingress>{release ? ingress : ""}</Ingress>
+      <SpacedDivider></SpacedDivider>
       <ReleaseView release={release} />
     </PageContainer>
   );
