f087ffc654fb41d232974a9c22f7b1434d5a1882
Lars <larsoyvind28@gmail.com>
2020-03-11 11:44:41 +0100

    KILL articles redux

--- src/reducers/combinedReducers.js
+++ src/reducers/combinedReducers.js
@@ -1,5 +1,4 @@
 import { combineReducers } from "redux";
-import { articlesReducer } from "./articlesReducer";
 import { productsReducer } from "./productsReducer";
 import { productVersionsReducer } from "./productVersionsReducer";
 import { loadingReducer } from "../slices/loadingSlice";
@@ -9,7 +8,6 @@ import { errorReducer } from "../slices/errorSlice";
 import { authReducer } from "../slices/authSlice";
 
 export default combineReducers({
-  articles: articlesReducer,
   products: productsReducer,
   productVersions: productVersionsReducer,
   releaseNotes: releaseNoteReducer,
