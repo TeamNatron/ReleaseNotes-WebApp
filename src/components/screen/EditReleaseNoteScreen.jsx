3e40113cc08ebac24a8a065ae49661d786283f1d
Lars <larsoyvind28@gmail.com>
2020-03-17 17:02:37 +0100

    conditionally put or save in "handleSave"

--- src/components/screen/EditReleaseNoteScreen.jsx
+++ src/components/screen/EditReleaseNoteScreen.jsx
@@ -4,7 +4,11 @@ import { Box } from "@material-ui/core";
 import { useHistory } from "react-router";
 import { useDispatch, useSelector } from "react-redux";
 import PageContainer from "../shared/PageContainer";
-import { fetchReleaseNoteById, putReleaseNote } from "../../slices/releaseNoteSlice";
+import {
+  fetchReleaseNoteById,
+  putReleaseNote,
+  postReleaseNote
+} from "../../slices/releaseNoteSlice";
 
 const EditReleaseNoteScreen = props => {
   const id = props.match.params.id;
@@ -19,10 +23,13 @@ const EditReleaseNoteScreen = props => {
     if (!note) {
       dispatch(fetchReleaseNoteById(id));
     }
-  }, []);
+  }, [dispatch, note, id]);
 
   const handleSave = objectToSave => {
-    dispatch(putReleaseNote(id, objectToSave));
+    // if screen has an id, a release is being updated
+    // otherwise, a release is being created
+    if (id) dispatch(putReleaseNote(id, objectToSave));
+    else dispatch(postReleaseNote(objectToSave));
   };
 
   const handleCancel = () => {
