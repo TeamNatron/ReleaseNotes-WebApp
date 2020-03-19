75b7b8456148f6c52248204f8558583fc3eb9d3a
Lars <larsoyvind28@gmail.com>
2020-03-19 16:45:39 +0100

    test that each screen renders

---
+++ src/components/screen/LandingScreen.test.js
@@ -0,0 +1,7 @@
+import { testScreenConformance } from "../../utils/test/testScreenConformance";
+import React from "react";
+import LandingScreen from "./LandingScreen";
+
+describe("<LandingScreen />", () => {
+  testScreenConformance(<LandingScreen />);
+});
---
+++ src/components/screen/ReleaseNoteEditorScreen.test.js
@@ -0,0 +1,7 @@
+import { testScreenConformance } from "../../utils/test/testScreenConformance";
+import React from 'react';
+import ReleaseNoteEditorScreen from "./ReleaseNoteEditorScreen";
+
+describe("<ReleaseNoteEditorScree  />", () => {
+  testScreenConformance(<ReleaseNoteEditorScreen />);
+});
