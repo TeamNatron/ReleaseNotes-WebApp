766f6f5bfd2eddfb165070cef852df6104327d05
Lars <larsoyvind28@gmail.com>
2020-03-21 17:51:50 +0100

    update productActions test

--- src/actions/productActions.test.js
+++ src/actions/productActions.test.js
@@ -1,27 +1,6 @@
-import configureMockStore from "redux-mock-store";
-import expect from "expect"; // You can use any testing library
-import thunk from "redux-thunk";
-import Axios from "axios";
-import { fetchProducts, actions } from "./productActions";
-
-const middlewares = [thunk];
-
-const mockStore = configureMockStore(middlewares);
-jest.mock("axios");
+import { fetchProducts } from "./productActions";
+import { testThunkConformance } from "../utils/test/testThunkConformance";
 
 describe("async actions", () => {
-  it("creates FETCH_PRODUCTS_PENDING and FETCH_PRODUCTS_SUCCESS after fetching products", async () => {
-    const data = {name: 'Cordel Inne'}
-    Axios.get.mockImplementationOnce(() => Promise.resolve(data));
-
-    const expectedActions = [
-        actions.fetchProductsPending(),
-        actions.fetchProductsSuccess()
-    ]
-    const store = mockStore({ products: [] })
-    return store.dispatch(fetchProducts()).then(() => {
-      // return of async actions
-      expect(store.getActions()).toEqual(expectedActions);
-    });
-  });
+  testThunkConformance(fetchProducts);
 });
