1f4932bd9816551252c9c9f721396b8f6b7349c0
Mordi490 <jananton007@hotmail.com>
2020-03-26 21:11:19 +0100

    Savant

--- src/components/adminpanel/AzureDevopsView.jsx
+++ src/components/adminpanel/AzureDevopsView.jsx
@@ -1,12 +1,71 @@
 import React from "react";
 import DevOpsForm from "./DevOpsForm";
+import { Description } from "@material-ui/icons";
+import AdminExpansionPanelBase from "../shared/AdminExpansionPanelBase";
+import ProjectSelector from "./ProjectSelector";
+import Divider from '@material-ui/core/Divider';
 
-const AzureDevOpsView = props => {
+// EXAMPLE COMPONENT USED TO CONTAIN AZURE DEVOPS TOOLS
+// CAN BE MODYFIED OR REMOVED
+const AzureDevOpsView = () => {
+  const createData = (name, id, isPublic) => {
+    return { name, id, isPublic };
+  };
 
+  const getDummyReleases = () => {
+    var releasesRows = [];
+    dummyReleases.forEach(r => {
+      releasesRows.push(createData(r.title, r.id));
+    });
+    return releasesRows;
+  };
+
+  const dummyReleases = [
+    { id: 200, title: "Azure Release 2.2" },
+    { id: 20, title: "Azure Release 3.2" },
+    { id: 202, title: "Azure Release 2.3" },
+    { id: 201, title: "Azure Release 6.2" },
+    { id: 99, title: "Azure Release 5.2" }
+  ];
+  const getDummyProjects = () => {
+    var projects = [];
+    dummyProjects.forEach(p => {
+      projects.push(p.title);
+    });
+    return projects;
+  };
+
+  const dummyProjects = [
+    { id: 200, title: "Azure Release 2.2" },
+    { id: 20, title: "Azure Release 3.2" },
+    { id: 202, title: "Azure Release 2.3" },
+    { id: 201, title: "Azure Release 6.2" },
+    { id: 99, title: "Azure Release 5.2" }
+  ];
+
+  const handleAction = (action, id) => {
+    switch (action) {
+      case "IMPORT":
+        postReleaseById();
+    }
+  };
+
+  const postReleaseById = () => {
+    console.log("Yes, da lag vi den!");
+  };
 
   return (
     <React.Fragment>
       <DevOpsForm></DevOpsForm>
+      <AdminExpansionPanelBase
+        expanded
+        label="Azure Devops Releases"
+        summaryComponent={<ProjectSelector projects={getDummyProjects()} />}
+        icon={<Description />}
+        rows={getDummyReleases()}
+        onAction={handleAction}
+        import={true}
+      ></AdminExpansionPanelBase>
     </React.Fragment>
   );
 };
