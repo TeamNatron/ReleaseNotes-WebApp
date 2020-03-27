d7c09b08c1b95c264699662e881739a2ebe010ab
Markus Randa <randamarkus.95@gmail.com>
2020-03-27 14:31:56 +0100

    Added new thunk for fetching releases.

--- src/slices/azureSlice.js
+++ src/slices/azureSlice.js
@@ -2,7 +2,7 @@ import { createReducer, createAction } from "@reduxjs/toolkit";
 import GlobalAxios from "axios";
 
 const Axios = GlobalAxios.create({
-  baseURL: "https://dev.azure.com/ReleaseNoteSystem/_apis/",
+  // baseURL: "https://dev.azure.com/ReleaseNoteSystem/_apis/",
   timeout: 1000
 });
 
@@ -12,11 +12,19 @@ export const getProjectsPending = createAction(name + "getProjectsPending");
 export const getProjectsError = createAction(name + "getProjectsError");
 export const getProjectsSuccess = createAction(name + "getProjectsSuccess");
 
+export const getReleasesPending = createAction(name + "getReleasesPending");
+export const getReleasesError = createAction(name + "getReleasesError");
+export const getReleasesSuccess = createAction(name + "getReleasesSuccess");
+
 // reducer
 export const azureReducer = createReducer(
-  { projects: [], releases: [] },
+  { sucessMsg: "", projects: [], releases: [] },
   {
-    [getProjectsSuccess]: (state, action) => {}
+    [getProjectsSuccess]: (state, action) => {},
+    [getReleasesSuccess]: (state, action) => {
+      state.sucessMsg = action.payload.statusText;
+      state.releases = action.payload.data;
+    }
   }
 );
 
@@ -30,4 +38,21 @@ export const fetchProjects = dispatch => {
     .catch(error => {
       dispatch(getProjectsError(error));
     });
+};
+
+export const fetchReleases = async (dispatch, project, organization) => {
+  dispatch(getReleasesPending());
+  Axios.get(
+    "https://vsrm.dev.azure.com/" +
+      organization +
+      "/" +
+      project +
+      "/_apis/release/releases?api-version=5.1"
+  ).then(res => {
+    dispatch(
+      getReleasesSuccess({ data: res.data, statusText: res.statusText })
+    ).catch(err => {
+      getReleasesError(err);
+    });
+  });
 };
