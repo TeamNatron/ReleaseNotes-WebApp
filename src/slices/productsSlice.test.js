b5ab818b684926307cbddd0446cbe7cdba26cd68
Markus Randa <randamarkus.95@gmail.com>
2020-04-01 10:32:51 +0200

    Created a selector to get all productVersions

--- src/slices/productsSlice.test.js
+++ src/slices/productsSlice.test.js
@@ -1,7 +1,8 @@
 import {
   productsReducer,
   fetchProductsSuccess,
-  fetchProducts
+  fetchProducts,
+  getProductVersions
 } from "./productsSlice";
 import { testThunkConformance } from "../utils/test/testThunkConformance";
 
@@ -21,3 +22,73 @@ describe("products reducer", () => {
 describe("products thunks", () => {
   testThunkConformance(fetchProducts);
 });
+
+describe("products selectors", () => {
+  it("should return all productVersions", async () => {
+    var productVersions = getProductVersions(dummyProducts);
+    expect(productVersions).toEqual(dummyProductVersions);
+  });
+});
+
+// DUMMYDATA
+const dummyProductVersions = [
+  {
+    id: 100,
+    version: "1.0",
+    fullName: "Cordel Inne 1.0",
+    value: "Cordel Inne 1.0"
+  },
+  {
+    id: 101,
+    version: "2.0",
+    fullName: "Cordel Inne 2.0",
+    value: "Cordel Inne 2.0"
+  },
+  {
+    id: 105,
+    version: "2.1-Beta",
+    fullName: "Cordel Inne 2.1-Beta",
+    value: "Cordel Inne 2.1-Beta"
+  },
+  {
+    id: 102,
+    version: "3.0",
+    fullName: "Cordel Ute 3.0",
+    value: "Cordel Ute 3.0"
+  }
+];
+
+const dummyProducts = [
+  {
+    id: 100,
+    name: "Cordel Inne",
+    versions: [
+      {
+        id: 100,
+        version: "1.0",
+        fullName: "Cordel Inne 1.0"
+      },
+      {
+        id: 101,
+        version: "2.0",
+        fullName: "Cordel Inne 2.0"
+      },
+      {
+        id: 105,
+        version: "2.1-Beta",
+        fullName: "Cordel Inne 2.1-Beta"
+      }
+    ]
+  },
+  {
+    id: 101,
+    name: "Cordel Ute",
+    versions: [
+      {
+        id: 102,
+        version: "3.0",
+        fullName: "Cordel Ute 3.0"
+      }
+    ]
+  }
+];
