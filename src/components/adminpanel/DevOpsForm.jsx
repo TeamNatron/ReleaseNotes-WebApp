f85e95957402bb59b2f49cc9ceff3718c443cc37
Mordi490 <jananton007@hotmail.com>
2020-04-02 20:41:38 +0200

    123

--- src/components/adminpanel/DevOpsForm.jsx
+++ src/components/adminpanel/DevOpsForm.jsx
@@ -1,81 +1,78 @@
 import React from "react";
-import DevOpsForm from "./DevOpsForm";
-import { Description } from "@material-ui/icons";
-import AdminExpansionPanelBase from "../shared/AdminExpansionPanelBase";
-import { Grid } from "@material-ui/core";
-import GeneralSelector from "../shared/GeneralSelector";
-import { useSelector, useDispatch } from "react-redux";
-import { useEffect } from "react";
-import {
-  getAllProductVersionsSelector,
-  fetchProducts
-} from "../../slices/productsSlice";
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import { Grid, Paper } from "@material-ui/core";
+import styled from "styled-components";
+import { useState } from "react";
+import Typography from '@material-ui/core/Typography';
+import { updateAzureInfo } from "../../slices/authSlice";
+import { useDispatch } from "react-redux";
 
-// EXAMPLE COMPONENT USED TO CONTAIN AZURE DEVOPS TOOLS
-// CAN BE MODYFIED OR REMOVED
-const AzureDevOpsView = props => {
-  const {
-    azureReleases,
-    azureProjects,
-    handleSelectedProject,
-    selectedProject,
-    handleImport,
-    selectedProductVersion,
-    handleSelectedProductVersion
-  } = props;
+
+const DevOpsForm = props => {
+  const [name, setName] = useState("");
+  const [PAT, setPAT] = useState("");
+  const [org, setOrg] = useState("");
   const dispatch = useDispatch();
 
-  const productVersions = useSelector(getAllProductVersionsSelector);
-
-  useEffect(() => {
-    dispatch(fetchProducts);
-  }, [dispatch]);
-
-  const handleAction = (action, id, data) => {
-    switch (action) {
-      case "IMPORT":
-        handleImport(id, data);
-        break;
-      default:
-        break;
-    }
+  const handleSubmit = () => {
+    // see if the fields have beeen filled out
+    if (name === "" || PAT === "" || org === "") {
+      alert("felt mangler innhold")
+    } else
+      dispatch(updateAzureInfo(name, PAT, org))
   };
 
   return (
     <React.Fragment>
-      <DevOpsForm />
-      <AdminExpansionPanelBase
-        expanded
-        label="Azure Devops Releases"
-        summaryComponent={
-          <Grid container>
-            <Grid item>
-              <GeneralSelector
-                items={azureProjects}
-                selected={selectedProject}
-                handleChange={handleSelectedProject}
-                label="Prosjekt"
-                helperText="Velg et prosjekt"
-              />
-            </Grid>
-            <Grid item>
-              <GeneralSelector
-                items={productVersions}
-                selected={selectedProductVersion}
-                handleChange={handleSelectedProductVersion}
-                label="Produkt"
-                helperText="Velg et produkt å importere til"
-              />
-            </Grid>
+      <MyPaper>
+        <Typography gutterBottom variant="h4">Azure Integrasjon</Typography>
+        <MyGrid container spacing={0} direction="column" justify="center" alignContent="space-between">
+          <Grid item>
+            <TextField id="standard-required DevOpsName"
+              label="DevOps Brukernavn"
+              value={name}
+              onChange={(e) => setName(e.target.value)} />
           </Grid>
-        }
-        icon={<Description />}
-        rows={azureReleases}
-        onAction={handleAction}
-        import={true}
-      ></AdminExpansionPanelBase>
+          <Grid item>
+            <TextField id="standard-required DevOpsPAT" label="Personal Access Token"
+              value={PAT}
+              onChange={(e) => setPAT(e.target.value)} />
+          </Grid>
+          <Grid item>
+            <TextField id="standard-required DevOpsOrg" label="DevOps Organisasjon"
+              value={org}
+              onChange={(e) => setOrg(e.target.value)} />
+          </Grid>
+          <Grid item>
+            <MyButton variant="contained" color="primary"
+              id="sendDataBtn" onClick={handleSubmit}>SEND</MyButton>
+          </Grid>
+        </MyGrid>
+      </MyPaper>
     </React.Fragment>
   );
 };
 
-export default AzureDevOpsView;
+export default DevOpsForm;
+
+const MyButton = styled(Button)`
+&& {
+    display: box;
+    float: left;
+    margin: 1vh 0 0 0;
+}
+`;
+
+const MyGrid = styled(Grid)`
+&& {
+    margin: 0 1vh;
+}
+`;
+
+const MyPaper = styled(Paper)`
+&& {
+    margin: 1vh 0;
+    padding: 1.2em;
+}
+`;
