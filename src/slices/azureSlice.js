88ff11970bee2cc7062b3c94332bb3c3e35c898c
Lars <larsoyvind28@gmail.com>
2020-04-16 18:44:50 +0200

    Clean up the import release promise chain

--- src/slices/azureSlice.js
+++ src/slices/azureSlice.js
@@ -4,7 +4,7 @@ import { postRelease } from "./releaseSlice";
 
 // azure axios instance
 export const AzureAxios = GlobalAxios.create({
-  timeout: 5000
+  timeout: 5000,
 });
 
 const API_VERSION = "&api-version=5.1";
@@ -28,12 +28,12 @@ export const azureReducer = createReducer(
   { sucessMsg: "", projects: [], releases: [] },
   {
     [getProjectsSuccess]: (state, action) => {
-      state.projects = action.payload.data.value.map(proj => proj.name);
+      state.projects = action.payload.data.value.map((proj) => proj.name);
     },
     [getReleasesSuccess]: (state, action) => {
       state.sucessMsg = action.payload.statusText;
       state.releases = action.payload.data.value;
-    }
+    },
   }
 );
 
@@ -55,7 +55,7 @@ export const azureReducer = createReducer(
  *          const params = useSelector(azureApiSelector)
  *          useDispatch(fetchProjects(params))
  */
-export const fetchProjects = params => async dispatch => {
+export const fetchProjects = (params) => async (dispatch) => {
   // create request url
   const instance = "dev.azure.com/" + params.organization;
   const url = "https://" + instance + "/_apis/projects";
@@ -63,10 +63,10 @@ export const fetchProjects = params => async dispatch => {
   //dispatch
   dispatch(getProjectsPending());
   AzureAxios.get(url, authHeader(params.authToken))
-    .then(res => {
+    .then((res) => {
       dispatch(getProjectsSuccess({ data: res.data }));
     })
-    .catch(error => {
+    .catch((error) => {
       dispatch(getProjectsError(error));
     });
 };
@@ -81,7 +81,7 @@ export const fetchProjects = params => async dispatch => {
  *
  */
 
-export const fetchReleases = params => async dispatch => {
+export const fetchReleases = (params) => async (dispatch) => {
   // create request url
   const instance =
     "vsrm.dev.azure.com/" + params.organization + "/" + params.project;
@@ -92,12 +92,10 @@ export const fetchReleases = params => async dispatch => {
     //dispatch
     dispatch(getReleasesPending());
     AzureAxios.get(url, authHeader(authToken))
-      .then(res => {
-        dispatch(
-          getReleasesSuccess({ data: res.data, statusText: res.statusText })
-        );
+      .then((res) => {
+        dispatch(getReleasesSuccess({ data: res.data }));
       })
-      .catch(err => {
+      .catch((err) => {
         getReleasesError(err);
       });
   }
@@ -107,11 +105,11 @@ export const fetchReleases = params => async dispatch => {
  * @param {String} authToken
  * @example Axios.get(url, authHeader("9u0fu8u94utgj03=="))
  */
-const authHeader = authToken => {
+const authHeader = (authToken) => {
   return {
     headers: {
-      Authorization: "Basic " + authToken
-    }
+      Authorization: "Basic " + authToken,
+    },
   };
 };
 
@@ -179,49 +177,65 @@ export const importRelease = (
   params,
   id,
   title
-) => async dispatch => {
+) => async (dispatch) => {
   dispatch(importReleasePending());
-  try {
-    // Get Ids of work items
-    fetchWorkItemIds(project, params, id).then(res => {
+
+  // Get Ids of work items
+  fetchWorkItemIds(project, params, id)
+    .then((res) => {
       var ids = res.data.value.map(({ id }) => id);
 
       // Get data of each work item
-      fetchWorkItems(project, params, ids).then(res => {
-        var rawWorkItems = res.data.value;
-        var workItems = [];
+      fetchWorkItems(project, params, ids)
+        .then((res) => {
+          var rawWorkItems = res.data.value;
+          var workItems = [];
 
-        // Format and filter each work item
-        rawWorkItems.forEach(wi => {
-          workItems.push(createWorkItem(wi));
+          // Format and filter each work item
+          rawWorkItems.forEach((wi) => {
+            workItems.push(createWorkItem(wi));
+          });
+
+          // Create new release
+          return {
+            title: title,
+            isPublic: false,
+            productVersionId: productVersionId,
+            releaseNotes: workItems,
+          };
+        })
+        .then((release) => {
+          // post new release
+          dispatch(importReleaseSuccess());
+          return release;
+        })
+        .then((release) => {
+          //success
+          return dispatch(postRelease(release));
+        })
+        .catch((err) => {
+          throw err;
         });
-
-        // Create new release and post
-        const release = {
-          title: title,
-          isPublic: false,
-          productVersionId: productVersionId,
-          releaseNotes: workItems
-        };
-
-        // Post release
-        dispatch(postRelease(release));
-        dispatch(importReleaseSuccess());
-      });
-    });
-  } catch (err) {
-    dispatch(importReleaseError(err));
-  }
+    })
+    .catch((err) =>
+      dispatch(
+        importReleaseError({
+          message:
+            "En feil oppstod ved innhenting av release fra azure: " +
+            err.message,
+        })
+      )
+    );
 };
 
 // util
-export const createWorkItem = item => {
+export const createWorkItem = (item) => {
   return {
     WorkItemId: item.id,
     WorkItemTitle: item.fields["System.Title"],
     ClosedDate: item.fields["System.CreatedDate"],
     WorkItemDescriptionHtml: item.fields["System.Description"],
     AuthorName: item.fields["System.CreatedBy"]["displayName"],
-    AuthorEmail: item.fields["System.CreatedBy"]["uniqueName"]
+    AuthorEmail: item.fields["System.CreatedBy"]["uniqueName"],
   };
 };
