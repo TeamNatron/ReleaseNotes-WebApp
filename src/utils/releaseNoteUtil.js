f6e06a7543006b9a2293c37cfc3fe2b23b137f46
Mordi490 <jananton007@hotmail.com>
2020-05-01 12:28:53 +0200

    updated noteUtil

--- src/utils/releaseNoteUtil.js
+++ src/utils/releaseNoteUtil.js
@@ -1,11 +1,34 @@
 export const classifyReleaseNote = (note) => {
   const { title, ingress, description } = note;
-  const [hasTitle, hasIngress] = [
+  const [hasTitle, hasIngress, hasDescription] = [
     title && title.length > 0,
     ingress && ingress.length > 0,
-    description && description.length > 0,
+    description && hasText(description),
   ];
-  if (!hasIngress && !hasTitle) {
-    return "DENSE";
-  } else return "FULL";
+  if (hasTitle) return "FULL";
+  return "DENSE";
+};
+
+/**
+ * check if html string has content
+ * @param {string} str the html string
+ */
+const hasText = (str) => {
+  if (str === "" || !str) return false;
+  const aux = document.createElement("div");
+  aux.innerHTML = str; //parses the html
+
+  var hasText = false;
+  aux.childNodes.forEach((child) => {
+    if (child.innerHTML?.trim().length > 0) {
+      hasText = true;
+    }
+    if (child.nodeName === "IMG") {
+      hasText = true;
+    }
+    if (child.TEXT_NODE) {
+      if (child.textContent.length > 0) hasText = true;
+    }
+  });
+  return hasText;
 };
